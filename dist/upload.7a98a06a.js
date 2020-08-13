b9c3021b512f8243de8b696f2ce4819a0d6b7f72
Uforo Ibenge <benjamin.uforo@gmail.com>
2020-08-13 15:15:29 +0100

    fix - styling and js

--- dist/upload.7a98a06a.js
+++ dist/upload.7a98a06a.js
@@ -217,11 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< Updated upstream
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50081" + '/');
-=======
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "53918" + '/');
->>>>>>> Stashed changes
 
   ws.onmessage = function (event) {
     checkedAssets = {};
